b7293ac184160c0ae9077d2f0bd1f8b010ed55d3
marineco <marinecolonge@gmail.com>
2017-01-17 11:26:08 +0100

    affichage des différentes propriétés de la nouvelle voiture

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-require 'lib/Car.php';
-require 'lib/MyCar.php';
-$car = new MyCar();
+	require 'lib/Car.php';
+	require 'lib/MyCar.php';
+
+	$car = new MyCar('4', 'Tesla', '5000', 'true', 'true', '5');
+
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,7 +12,12 @@ $car = new MyCar();
 </head>
 <body>
 	<h1>My Car</h1>
-<p><?= $car->getManufacturer() ?></p>
+	<p><?= $car->getWheels() ?></p>
+	<p><?= $car->getManufacturer() ?></p>
+	<p><?= $car->getKilometrage() ?></p>
+	<p><?= $car->getAirbag() ?></p>
+	<p><?= $car->getAbs() ?></p>
+	<p><?= $car->getDoors() ?></p>
 	
 </body>
 </html>
