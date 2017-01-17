72356d9fd5c03fbb49f282286edd4c8ab2b846f3
marineco <marinecolonge@gmail.com>
2017-01-17 11:24:47 +0100

    création d'un constructeur ds lequel sont définis les propriétés de la voiture

--- lib/MyCar.php
+++ lib/MyCar.php
@@ -1,5 +1,13 @@
 <?php
 
 class MyCar extends Car {
-	
+
+	public function __construct($nbWheels, $name, $km, $available, $available2, $nbDoor) {
+		parent::setWheels($nbWheels);
+		parent::setManufacturer($name);
+		parent::setKilometrage($km);
+		parent::setAirbag($available);
+		parent::setAbs($available2);
+		parent::setDoors($nbDoor);
+	}
 }
